255c95324fb25ad033d40721d37b62cbfb8a58e2
Bega-pixel <bega2000@protonmail.com>
2022-10-21 11:38:33 +0200

    js ej2

---
+++ index.js
@@ -0,0 +1,18 @@
+const nombre = "Jeremy";
+const edad = 24;
+const desarrolador = true;
+const fecha = new Date(2022,2,15);
+const libro = {
+    titulo: "Le Petit Prince",
+    autor: " Antoine de Saint-Exupéry",
+    fecha: "publicación 6 de abril de 1943",
+    url: "https://es.wikipedia.org/wiki/El_principito"
+};
+
+console.log("Un nombre:" + typeof nombre);
+console.log("Una edad" + typeof edad);
+console.log("¿Eres desarrollador?" + typeof desarrolador);
+console.log("una fecha: " + typeof fecha);
+console.log("un libro:" + typeof libro);
+
+
